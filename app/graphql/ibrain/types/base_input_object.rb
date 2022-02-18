39a935d70bcfd01d60b4582f38ac58e97e983264
John Nguyen <49914814+john-techfox@users.noreply.github.com>
2022-02-18 10:13:27 +0700

    add base input object

--- app/graphql/ibrain/types/base_input_object.rb
+++ app/graphql/ibrain/types/base_input_object.rb
@@ -4,6 +4,12 @@ module Ibrain
   module Types
     class BaseInputObject < GraphQL::Schema::InputObject
       argument_class Ibrain::Types::BaseArgument
+
+      def to_params
+        ActionController::Parameters.new(
+          to_h.with_indifferent_access.transform_keys(&:underscore)
+        )
+      end
     end
   end
 end

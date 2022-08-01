73a542fb28c700494e49dc24649ae3195318a1a6
Tai Nguyen <tainv@its-global.vn>
2022-08-01 15:10:22 +0700

    change version

--- lib/ibrain/core/version.rb
+++ lib/ibrain/core/version.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 module Ibrain
-  VERSION = "0.3.8"
+  VERSION = "0.3.9"
 
   def self.ibrain_version
     VERSION
   end
 
   def self.previous_ibrain_minor_version
-    '0.3.7.rc-1'
+    '0.3.8'
   end
 
   def self.ibrain_gem_version

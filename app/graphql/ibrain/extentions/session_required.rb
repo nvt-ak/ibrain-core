5b57ce1e6c546ab72607bcd6f585abaedede3ee8
tungvt193 <20160584@student.hust.edu.vn>
2023-05-30 15:37:26 +0700

    update remove token

--- app/graphql/ibrain/extentions/session_required.rb
+++ app/graphql/ibrain/extentions/session_required.rb
@@ -4,7 +4,10 @@ module Ibrain
   module Extentions
     class SessionRequired < GraphQL::Schema::FieldExtension
       def resolve(object:, arguments:, **rest)
-        raise ActionController::InvalidAuthenticityToken, I18n.t('ibrain.errors.session.invalid_session') if is_invalid_session(object)
+        if is_invalid_session(object)
+          Device.find_by_token(device_token(object))&.destroy!
+          raise ActionController::InvalidAuthenticityToken, I18n.t('ibrain.errors.session.invalid_session')
+        end
 
         # yield the current time as `memo`
         yield(object, arguments, rest)
@@ -14,6 +17,10 @@ module Ibrain
 
       def is_invalid_session(object)
         object.try(:context).try(:fetch, :current_user, nil).blank? && options.try(:fetch, :session_required, false)
+      end
+
+      def device_token(object)  
+        object.try(:context).try(:fetch, :request, nil).headers["Device-token"]
       end
     end
   end
